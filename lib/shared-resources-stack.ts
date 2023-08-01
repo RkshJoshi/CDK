fe06205615f10b8ffe73112c85254d60f46c5edb
Rakesh Joshi <rakesh.joshi@versent.com.au>
2023-08-01 13:02:48 +1000

    added rds resources in the stack

--- lib/shared-resources-stack.ts
+++ lib/shared-resources-stack.ts
@@ -6,6 +6,7 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as kms from "aws-cdk-lib/aws-kms";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as efs from "aws-cdk-lib/aws-efs";
+import * as rds from "aws-cdk-lib/aws-rds";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface baseProperties extends cdk.StackProps {
@@ -23,9 +24,12 @@ export class sharedResourcesStack extends cdk.Stack {
     const kmsKeyId = props.kmsKeyId;
 
     const d_vpc = ec2.Vpc.fromLookup(this, "deployVPC", {
-      vpcId: "vpc-0fb37f3d333d536de",
       vpcName: `*${props.env?.account}*`,
     });
+
+    const privateSubnets = d_vpc.publicSubnets; //TODO: change this to private subnets, I don't have private subnets in default vpc
+
+    console.log(privateSubnets);
 
     const efsSecurityGroup = new ec2.SecurityGroup(this, "efsSG", {
       vpc: d_vpc,
@@ -38,7 +42,84 @@ export class sharedResourcesStack extends cdk.Stack {
       enableAutomaticBackups: true,
       fileSystemName: `ckan-efs-${props.envName}`,
       kmsKey: kmsKeyId,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
       securityGroup: efsSecurityGroup,
+    });
+
+    const efsCkanAccessPoint = new efs.AccessPoint(this, "ckanAccessPoint", {
+      fileSystem: efsVolume,
+      path: "/var/lib/ckan",
+      createAcl: {
+        ownerGid: "1000",
+        ownerUid: "1000",
+        permissions: "750",
+      },
+      posixUser: {
+        uid: "1000",
+        gid: "1000",
+      },
+    });
+
+    const efsSolrAccesPoint = new efs.AccessPoint(this, "solrAccessPoint", {
+      fileSystem: efsVolume,
+      path: "/var/solr",
+      createAcl: {
+        ownerGid: "2000",
+        ownerUid: "2000",
+        permissions: "750",
+      },
+      posixUser: {
+        uid: "2000",
+        gid: "2000",
+      },
+    });
+
+    const rdsSG = new ec2.SecurityGroup(this, "rdsSG", {
+      vpc: d_vpc,
+      allowAllOutbound: true,
+      securityGroupName: `rdsSG`,
+      description: "Security group for the RDS",
+    });
+
+    const rdsSubnetGroup = new rds.SubnetGroup(this, "rdsSubnetGroup", {
+      description: "SubnetGroup for the ckan RDS",
+      vpc: d_vpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      vpcSubnets: {
+        availabilityZones: [
+          "ap-southeast-2a",
+          "ap-southeast-2a",
+          "ap-southeast-2c",
+        ],
+        onePerAz: true,
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+    });
+
+    const ckanRdsCluster = new rds.DatabaseCluster(this, "rdsCluster", {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_03_0,
+      }),
+      backup: {
+        retention: cdk.Duration.days(30),
+        preferredWindow: "01:00-02:00",
+      },
+      copyTagsToSnapshot: true,
+      serverlessV2MaxCapacity: 2,
+      serverlessV2MinCapacity: 1,
+      vpc: d_vpc,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      subnetGroup: rdsSubnetGroup,
+      writer: rds.ClusterInstance.serverlessV2("writer", {
+        allowMajorVersionUpgrade: true,
+        autoMinorVersionUpgrade: true,
+        enablePerformanceInsights: true,
+        instanceIdentifier: "ckan-db",
+        publiclyAccessible: false,
+      }),
+      readers: [
+        rds.ClusterInstance.serverlessV2("reader1", { scaleWithWriter: true }),
+      ],
     });
   }
 }
